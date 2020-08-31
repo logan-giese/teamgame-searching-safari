82d42f7b688388f83712421bea406f7187f60387
Garrett Wagner <Garrett.Wagner@eagles.oc.edu>
2020-08-31 13:44:23 -0500

    created cameraview prefab

---
+++ Assets/Scripts/creatureMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creatureMovement : MonoBehaviour
+{
+    private Vector3 spawnLocation;
+    private Vector3 currentLocation;
+
+    private Vector3 endLocation = new Vector3(-3f,0f,22f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get position
+        spawnLocation = Transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //if spwaned on left side
+
+       //else if spawned on right side
+
+       //else the tutorial for the player 
+    }
+}
