26bdfc54dedc18436cc5c181fc8731d803caa3ca
Garrett Wagner <Garrett.Wagner@eagles.oc.edu>
2020-08-31 13:54:38 -0500

    commented out code

--- Assets/Scripts/creatureMovement.cs
+++ Assets/Scripts/creatureMovement.cs
@@ -6,22 +6,31 @@ public class creatureMovement : MonoBehaviour
 {
     private Vector3 spawnLocation;
     private Vector3 currentLocation;
-
+    private 
     private Vector3 endLocation = new Vector3(-3f,0f,22f);
     // Start is called before the first frame update
     void Start()
     {
-        //get position
-        spawnLocation = Transform.position;
+        //roll for which side to spawn on
+        // randomSpawn();
+        // //get position
+        // spawnLocation = Transform.position;
+        // currentLocation = spawnLocation;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //get current location for the update
+
        //if spwaned on left side
 
        //else if spawned on right side
 
        //else the tutorial for the player 
     }
+    // private void randomSpawn()
+    // {
+    //     int roll = Random.rand();
+    // }
 }
