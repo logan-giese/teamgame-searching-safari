5d609c18bd52fda4b5b5d518004de3f486e138fd
Garrett Wagner <Garrett.Wagner@eagles.oc.edu>
2020-09-07 14:41:23 -0500

    Got spawning set up for the animator. have untested trigger for creatures
    Need to test the trigger event for the creatures. working on detecting if food and if correct food for animal. eventually need to work on reaction animations and then randomization of the animal.

--- Assets/Scripts/creatureMovement.cs
+++ Assets/Scripts/creatureMovement.cs
@@ -6,13 +6,16 @@ public class creatureMovement : MonoBehaviour
 {
     private Vector3 spawnLocation;
     private Vector3 currentLocation;
-    private Vector3 endLocation = new Vector3(-3f,0f,22f);
+    private Vector3 endLocations;
+    private Vector3 scale;
+    private bool isInArea = false;
+    private bool isCorrectFood = false;
     // Start is called before the first frame update
     void Start()
     {
-        //roll for which side to spawn on
-        randomSpawn();
-        //get position
+        // //roll for which side to spawn on
+        spawn();
+        // //get position
         spawnLocation = transform.position;
         currentLocation = spawnLocation;
     }
@@ -20,33 +23,71 @@ public class creatureMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //get current location for the update
-        currentLocation = transform.position;
-       //if spawned on left side
-       if(spawnLocation.x == -40f)
-        currentLocation.x += 5f * Time.deltaTime;
-       //else if spawned on right side
-       else if(spawnLocation.x == 40f)
-        currentLocation.x -= 5f * Time.deltaTime;
-       //else the tutorial for the player 
-
-       //set the new position
-        transform.position = currentLocation;
+        move();
     }
-    private void randomSpawn()
+    public void spawn()
     {
-        int roll = Random.Range(1,100);
+        int roll = Random.Range(1, 100);
         //if even left
-        if(roll % 2 == 0)
+        if (roll % 2 == 0)
         {
-            Debug.Log("Left");
-            transform.position = new Vector3(-40f, 0.5f,Random.Range(40f,60f));
+            // Debug.Log("Left Spawn");
+            transform.position = new Vector3(-40f, 1.2f, Random.Range(40f, 60f));
         }
         //else right
         else
         {
-            Debug.Log("Right");
-            transform.position = new Vector3(40f, 0.5f,Random.Range(30f,20f));
+            // Debug.Log("Right Spawn");
+            transform.position = new Vector3(40f, 1.2f, Random.Range(30f, 20f));
+        }
+    }
+    public void move()
+    {
+        //get current location for the update
+        currentLocation = transform.position;
+        //if spawned on left side
+        if (spawnLocation.x == -40f)
+            currentLocation.x += 5f * Time.deltaTime;
+        //else if spawned on right side
+        else if (spawnLocation.x == 40f)
+            currentLocation.x -= 5f * Time.deltaTime;
+        //else the tutorial for the player 
+
+        //set the new position
+        transform.position = currentLocation;
+    }
+    //scales for the different animals
+    //elephant: 3.6,1.9,1.5
+    public void setIsInArea(bool flag)
+    {
+        isInArea = flag;
+    }
+    public bool getIsInArea()
+    {
+        return isInArea;
+    }
+    public void setIsCorrectFood(bool flag)
+    {
+        isCorrectFood = flag;
+    }
+    public bool getIsCorrectFood()
+    {
+        return isCorrectFood;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit");
+        if (other.tag == "broccoli" || other.tag == "meat")
+        {
+            isInArea = true;
+            if (other.tag == "broccoli")
+            {
+                isCorrectFood = true;
+            }
+            else
+            {
+                isCorrectFood = false;
+            }
         }
     }
 }
