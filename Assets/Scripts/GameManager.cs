879b9cfcdc6b124f4e8b2a247141ca33f0d48f6d
Garrett Wagner <Garrett.Wagner@eagles.oc.edu>
2020-09-12 13:55:36 -0500

    Got the level change set up for loading levels
    This has level loading and even the functionality for the spawning of the other animals

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,16 +15,19 @@ public class GameManager : MonoBehaviour
     public GameObject Giraffe;
     public GameObject Lion;
     public GameObject Rhino;
-    private int[] Count = {0,0,0,0};
+    public GameObject Frog;
+    public GameObject Crocodile;
+    private int[] Count = {0,0,0,0,0,0};
     private bool allDestroyed = false;
+    private int level = 1;
     private float timer = 0f;
     //game manager will be told about the flag status by the creatures and be accessed by the character
     private int flag = -1;//-1 is no response,0 is wrong, 1 is correct
     // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
+    void Start()
+    {
+        Debug.Log("Begin");
+    }
 
     // Update is called once per frame
     void Update()
@@ -33,6 +36,8 @@ public class GameManager : MonoBehaviour
         if(timer >= (float)7f)
         {
             int roll = Random.Range(0,4);
+            if(level == 2)
+            roll = Random.Range(0,6);
             if(Count[roll] < 5)
             {
                 if(roll == 0)
@@ -55,11 +60,53 @@ public class GameManager : MonoBehaviour
                     //spawn Rhino
                     Instantiate(Rhino);
                 }
+                //if the level switched from 1 to 2
+                if(level == 2)
+                {
+                    if(roll == 4)
+                    {
+                        Instantiate(Frog);
+                    }
+                    else if(roll == 5)
+                    {
+                        Instantiate(Crocodile);
+                    }
+                }
                 Count[roll]++;
             }
 
             timer = 0f;
+            endCurrentLevel();
         }
+    }
+    public void endCurrentLevel()
+    {
+        int count = 0;
+        GameObject[] ar = GameObject.FindGameObjectsWithTag("Creature");
+        Debug.Log($"Level {level}");
+        foreach(int c in Count)
+        {
+            count += c;
+        }
+        if(level == 1)
+        {
+            //if the array size is 0 and the count is equal to 20 then change
+            if(ar.Length == 0 && count == 20)
+            {
+                Debug.Log($"Level {level} Finished");
+                level = 2;
+            }
+        }
+        if(level == 2)
+        {
+            //if the array size is 0 and the count is equal to 20 then change
+            if(ar.Length == 0 && count == 30)
+            {
+                Debug.Log($"Level {level} Finished");
+                level = 2;
+            }
+        }
+        
     }
     public int getFlag()
     {
